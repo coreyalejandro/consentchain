f9c3850a41b5026ca0dd31c2e083858b28c0a05b
Corey Alejandro <corey@coreyalejandro.com>
2026-03-11 13:45:30 -0500

    feat: add ProactiveDemo composition with TransitionSeries

---
+++ Projects/proactive-demo/src/ProactiveDemo.tsx
@@ -0,0 +1,42 @@
+import { TransitionSeries, linearTiming } from "@remotion/transitions";
+import { fade } from "@remotion/transitions/fade";
+import { SCENES, FADE_DURATION_FRAMES } from "./config/scenes";
+import { TitleCard } from "./scenes/TitleCard";
+import { VideoSegment } from "./scenes/VideoSegment";
+
+export const ProactiveDemo: React.FC = () => {
+  return (
+    <TransitionSeries>
+      {SCENES.map((scene, index) => {
+        const isLast = index === SCENES.length - 1;
+
+        return [
+          <TransitionSeries.Sequence
+            key={scene.id}
+            durationInFrames={scene.durationInFrames}
+          >
+            {scene.type === "title" ? (
+              <TitleCard title={scene.title} subtitle={scene.subtitle} />
+            ) : (
+              <VideoSegment
+                startSec={scene.startSec}
+                endSec={scene.endSec}
+                captions={scene.captions}
+                zoom={scene.zoom}
+              />
+            )}
+          </TransitionSeries.Sequence>,
+          !isLast ? (
+            <TransitionSeries.Transition
+              key={`${scene.id}-transition`}
+              presentation={fade()}
+              timing={linearTiming({
+                durationInFrames: FADE_DURATION_FRAMES,
+              })}
+            />
+          ) : null,
+        ];
+      })}
+    </TransitionSeries>
+  );
+};
